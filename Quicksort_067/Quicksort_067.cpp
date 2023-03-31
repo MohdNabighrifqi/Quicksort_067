ea7cce26bf3c44093716a0395674a107109c968c
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-31 15:10:27 +0700

    memasukan input

--- Quicksort_067/Quicksort_067.cpp
+++ Quicksort_067/Quicksort_067.cpp
@@ -7,4 +7,18 @@ int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of data movement
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "/nmaksimum panjang array adalah 20" << endl;
+
+	}
+
+}
 
